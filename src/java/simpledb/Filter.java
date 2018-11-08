87b366157e098b81f02859d439795bababfb93ba
魏昊然 <weihaoran@dhcp-wifi-8021x-155-41-70-0.bu.edu>
2018-11-08 14:17:47 -0500

    Filter finished

--- src/java/simpledb/Filter.java
+++ src/java/simpledb/Filter.java
@@ -8,6 +8,9 @@ import java.util.*;
 public class Filter extends Operator {
 
     private static final long serialVersionUID = 1L;
+    private Predicate p;
+    private DbIterator child;
+    private DbIterator[] children;
 
     /**
      * Constructor accepts a predicate to apply and a child operator to read
@@ -19,30 +22,40 @@ public class Filter extends Operator {
      *            The child operator
      */
     public Filter(Predicate p, DbIterator child) {
+        this.p = p;
+        this.child = child;
+        this.children = new DbIterator[1];
+        this.children[0] = child;
         // some code goes here
     }
 
     public Predicate getPredicate() {
         // some code goes here
-        return null;
+        return p;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return child.getTupleDesc();
     }
 
     public void open() throws DbException, NoSuchElementException,
             TransactionAbortedException {
+        super.open();
+        child.open();
         // some code goes here
     }
 
     public void close() {
         // some code goes here
+        child.close();
+        super.close();
+
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+        child.rewind();
     }
 
     /**
@@ -57,18 +70,25 @@ public class Filter extends Operator {
     protected Tuple fetchNext() throws NoSuchElementException,
             TransactionAbortedException, DbException {
         // some code goes here
+        while (child.hasNext()){
+            Tuple next = child.next();
+            if (p.filter(next)){
+                return next;
+            }
+        }
         return null;
     }
 
     @Override
     public DbIterator[] getChildren() {
         // some code goes here
-        return null;
+        return children;
     }
 
     @Override
     public void setChildren(DbIterator[] children) {
         // some code goes here
+        this.children = children;
     }
 
 }
